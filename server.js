eae8fcd0dad283527f949196db8086a915a9f53e
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-08 09:50:00 -0400

    server commit

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+// Dependencies
+var express = require("express");
+// var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Create express app instance.
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
 
