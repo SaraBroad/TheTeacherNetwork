bf4bf0b27d36e6e6f39df30414e8ef70c435fb37
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-09 17:18:25 -0400

    keys

---
+++ app/models/keys.js
@@ -0,0 +1,7 @@
+exports.twitter = {
+    consumer_key: process.env.aLXcxb7JJGZvjEbhWxdSl5ItM
+    consumer_secret: process.env.wvWvrsyt0YQifZa8RroL0G06BTbpX95hUXxgK6nn28WbOhLOYD,
+    access_token_key: process.env.745496905-5f2rlIP5WyuuBJVGdbGKuOuJVJdnHPaZfRF1WZXA,
+    access_token_secret: process.env.vDVWVNjfpewQQIfwuPl2fPbXxWHIXw2D7gxKrGSdIXzrf
+  };
+  
