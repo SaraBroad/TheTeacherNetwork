04234c78060d0b09c9bdeb242675c98cf764a433
Sara Broad <saraebroad@gmail.com>
2018-05-27 18:15:33 -0400

    model

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
     var Contact = sequelize.define("Contact", {
         name: DataTypes.STRING(45),
-        email: DataTypes.STRING(45)
+        email: DataTypes.STRING(45),
+        position: DataTypes.STRING(45)
     })
 }
